63b2fb6ec139657ab740d2930c681af3d4a6b95a
Carlos O <orjuelacarlos80@gmail.com>
2024-04-11 20:43:11 -0500

    first commit :)

---
+++ hola/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Casa(),
+    );
+  }
+}
+
+class Casa extends StatefulWidget {
+  const Casa({super.key});
+
+  @override
+  State<Casa> createState() => _CasaState();
+}
+
+class _CasaState extends State<Casa> {
+  String nombre = "ale";
+  var color = Colors.black;
+  @override
+  Widget build(BuildContext context) {
+    var texto = Text(
+      nombre,
+      style: TextStyle(color: color),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("pruebas"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+        child: GestureDetector(
+          child: texto,
+          onLongPress: () {
+            setState(() {
+              color = Colors.redAccent;
+            });
+          },
+          onDoubleTap: () {
+            setState(() {
+              color = Colors.greenAccent;
+            });
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        setState(() {
+          color = Colors.black;
+        });
+      }),
+    );
+  }
+}
